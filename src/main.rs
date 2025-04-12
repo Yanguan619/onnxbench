6ba4f77a71fec41ebf5cff57aa995a96023e1273
Yanguan619 <1131595527@qq.com>
2025-04-13 01:33:09 +0800

    Remove unused dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,21 @@
 #![allow(clippy::manual_retain)]
 
-use clap::{Parser, Subcommand};
-use image::{imageops::FilterType, GenericImageView};
+use clap::Parser;
 use indicatif::{ProgressBar, ProgressStyle};
-use ndarray::{s, Array, Axis, Dim, Dimension, IntoDimension};
-use ort::{
-    inputs,
-    session::{Session, SessionOutputs},
-    value::TensorRef,
-};
-use raqote::{DrawOptions, DrawTarget, LineJoin, PathBuilder, SolidSource, Source, StrokeStyle};
-use show_image::{event, AsImageView, WindowOptions};
-use std::{collections::HashMap, iter::Map, path::Path, time::Duration};
-use std::{process::exit, time::Instant};
-use tracing::{debug, info};
+use ort::session::Session;
+use std::time::Instant;
+use std::{collections::HashMap, time::Duration};
+use tracing::debug;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 mod common;
 
 #[derive(Parser)]
 #[command(
     version = "0.0.1",
-    about = "Toolpack is a collection of tools for Rust developers"
+    about = "onnxbench is a collection of tools for Rust developers"
 )]
 struct Cli {
-    #[arg(short, long, default_value = "0")]
-    verbose: u8,
-    #[arg(
-        short,
-        long,
-        default_value = "D:/mCloudDownload/dl/weights/yolo/yolov5nu.onnx"
-    )]
+    #[arg(short, long, default_value = ".assets/yolov5nu.onnx")]
     model_path: String,
     #[arg(short, long, default_value = "100")]
     loop_num: usize,
@@ -39,7 +25,26 @@ struct Cli {
     level: String,
 }
 
-fn benchmark(model_path: &str, loop_num: usize, input_shape: HashMap<String, Vec<usize>>) {
+impl Cli {
+    fn parse_input_shape(&self) -> HashMap<String, Vec<usize>> {
+        let mut input_shape: HashMap<String, Vec<usize>> = HashMap::new();
+        let x = self.input_shape.split(";").collect::<Vec<&str>>();
+        for i in 0..x.len() {
+            let temp: Vec<&str> = x[i].split(":").collect();
+            input_shape.insert(
+                temp[0].to_string(),
+                temp[1].split(",").map(|x| x.parse().unwrap()).collect(),
+            );
+        }
+        input_shape
+    }
+}
+
+fn benchmark(
+    model_path: &str,
+    loop_num: usize,
+    input_shape: HashMap<String, Vec<usize>>,
+) -> Result<(), usize> {
     common::init().unwrap();
 
     let model = Session::builder()
@@ -47,7 +52,6 @@ fn benchmark(model_path: &str, loop_num: usize, input_shape: HashMap<String, Vec
         .commit_from_file(model_path)
         .unwrap();
 
-    let mut exit_flag = true;
     for inputi in &model.inputs {
         debug!("input name : {:?}", inputi.name);
         debug!(
@@ -57,20 +61,23 @@ fn benchmark(model_path: &str, loop_num: usize, input_shape: HashMap<String, Vec
         );
     }
 
+    debug!("user input shape: {:?}", input_shape);
+
     // inference
     let mut costs: Vec<Duration> = Vec::new();
 
     let bar = ProgressBar::new(loop_num as u64);
     bar.set_style(
         ProgressStyle::default_bar()
-            .template("Infer: {bar:60} {pos:>}/{len} [{elapsed_precise}/{eta_precise}]")
+            .template("Steps {pos:>}/{len}: {bar:50} [{elapsed_precise}/{eta_precise}]")
             .unwrap(),
     );
+    println!("Start benchmark...");
     for _ in 0..loop_num {
         let input = ndarray::Array4::<f32>::zeros((1, 3, 640, 640));
 
         let start = Instant::now();
-        let outputs = model
+        let _outputs = model
             .run(
                 ort::inputs! {
                     "images" => input
@@ -88,10 +95,11 @@ fn benchmark(model_path: &str, loop_num: usize, input_shape: HashMap<String, Vec
     let cost_first = costs[0];
     let cost_last = costs.iter().last().unwrap();
     let cost_average = costs.iter().sum::<Duration>() / loop_num as u32;
-    info!(
-        "first elapsed: {:.2?}, average elapsed: {:.2?}, last elapsed: {:.2?}",
+    println!(
+        "End benchmark, first elapsed: {:.2?}, average elapsed: {:.2?}, last elapsed: {:.2?}.",
         cost_first, cost_average, cost_last
-    )
+    );
+    Ok(())
 }
 
 fn main() {
@@ -108,22 +116,23 @@ fn main() {
         .with(tracing_subscriber::fmt::layer())
         .init();
 
-    let mut input_shape: HashMap<String, Vec<usize>> = HashMap::new();
-    let x = cli.input_shape.split(";").collect::<Vec<&str>>();
-    for i in 0..x.len() {
-        let temp: Vec<&str> = x[i].split(":").collect();
-        input_shape.insert(
-            temp[0].to_string(),
-            temp[1].split(",").map(|x| x.parse().unwrap()).collect(),
-        );
-    }
+    let input_shape = cli.parse_input_shape();
     debug!("{:?}", input_shape);
 
-    benchmark(cli.model_path.as_str(), cli.loop_num, input_shape);
+    let _res = benchmark(cli.model_path.as_str(), cli.loop_num, input_shape);
 }
-
-// #[cfg(test)]
-// fn test_benchmark() {
-//     const YOLOV8M_URL: &str = "D:/mCloudDownload/dl/weights/yolo/yolov5nu.onnx";
-//     benchmark(YOLOV8M_URL);
-// }
+#[test]
+fn test_benchmark() {
+    let inn = Cli {
+        model_path: ".assets/yolov5nu.onnx".to_string(),
+        loop_num: 2,
+        level: "debug".to_string(),
+        input_shape: "images:1,3,640,640".to_string(),
+    };
+    let res = benchmark(
+        inn.model_path.as_str(),
+        inn.loop_num,
+        inn.parse_input_shape(),
+    );
+    assert_eq!(res, Ok(()));
+}
